51592be79817475c9d37cee3e68d59768f46d190
Nonojim <nolan.pederencino@gmail.com>
2024-01-11 15:35:20 +0100

    Logement et fiche logement

--- src/components/FicheLogement.jsx
+++ src/components/FicheLogement.jsx
@@ -1,12 +1,24 @@
 import DropdownMenu from './DropdownMenu';
 
-const FicheLogement = () => {
+const FicheLogement = ({title, location, cover, description, tags, equipments, host, rating}) => {
   return (
     <div>
-      <img></img>
-      <h1></h1>
-      <DropdownMenu />
-      <DropdownMenu />
+      <img loading="lazy" src={cover} alt={description} />
+      <div>
+        <h1>{title}</h1>
+        <h2>{location}</h2>
+      </div>
+      <div>
+        {host.name} <img src={host.picture} />
+      </div>
+      <div>
+        <div>{tags}</div>
+        <div>
+          <span>{rating}⭐</span>
+        </div>
+      </div>
+      <DropdownMenu title={'Description'} content={description} />
+      <DropdownMenu title={'Équipements'} content={equipments} />
     </div>
   );
 };
--- src/pages/Logements.jsx
+++ src/pages/Logements.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import FicheLogement from '../components/FicheLogement';
+import {useParams} from 'react-router';
+import LogementList from '../data/logements.json';
 
 const Logements = () => {
+  const {id} = useParams();
+  const logement = LogementList.find(logement => logement.id === id);
+
   return <FicheLogement {...logement} />;
 };
 
