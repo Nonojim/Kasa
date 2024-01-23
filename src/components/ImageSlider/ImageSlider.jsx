0709b0b268a08c519efb7ac98a258abecdcb573c
Nonojim <nolan.pederencino@gmail.com>
2024-01-23 19:48:27 +0100

    ImageSlider 2.0

--- src/components/ImageSlider/ImageSlider.jsx
+++ src/components/ImageSlider/ImageSlider.jsx
@@ -19,14 +19,38 @@ export default function ImageSlider({imageUrls}) {
   }
 
   return (
-    <div className="ImageSlider">
-      <img src={imageUrls[imageIndex]} className="img-slider-img" />
+    <div className="img-slider">
+      <div className="img-slider-imgcontainer">
+        {imageUrls.map(url => (
+          <img
+            key={url}
+            src={url}
+            className="img-slider-img"
+            style={{translate: `${-100 * imageIndex}%`}}
+          />
+        ))}
+      </div>
       <button onClick={showPrevImage} className="img-slider-btn" style={{left: 0}}>
         <span className="material-symbols-outlined">arrow_back_ios</span>
       </button>
       <button onClick={showNextImage} className="img-slider-btn" style={{right: 0}}>
         <span className="material-symbols-outlined">arrow_forward_ios</span>
       </button>
+      <div className="img-slider-bottombar">
+        {imageUrls.map((_, index) => (
+          <button
+            key={index}
+            className="img-slider-bottombar-btn"
+            onClick={() => setImageIndex(index)}
+          >
+            {index === imageIndex ? (
+              <span className="material-symbols-outlined">adjust</span>
+            ) : (
+              <span className="material-symbols-outlined">circle</span>
+            )}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
